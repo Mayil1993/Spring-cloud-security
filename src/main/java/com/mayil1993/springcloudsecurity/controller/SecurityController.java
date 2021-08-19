a34ff21ba18d7bc4649482c4eb218ec1133ee74f
Mayil Vahanan <mavah93@gmail.com>
2021-08-20 01:16:39 +0800

    Google Authenticator

---
+++ src/main/java/com/mayil1993/springcloudsecurity/controller/SecurityController.java
@@ -0,0 +1,22 @@
+package com.mayil1993.springcloudsecurity.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+
+@RestController
+public class SecurityController {
+
+    @GetMapping
+    public String welcome() {
+        return "Welcome to Google !!";
+    }
+
+    @GetMapping("/user")
+    public Principal user(Principal principal) {
+        System.out.println("username : " + principal.getName());
+        return principal;
+    }
+
+}
---
+++ src/test/java/com/mayil1993/springcloudsecurity/SpringCloudSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mayil1993.springcloudsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
